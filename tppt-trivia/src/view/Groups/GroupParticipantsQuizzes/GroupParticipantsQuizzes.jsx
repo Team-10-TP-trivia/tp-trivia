3affb11dc27609dd63aa8af13b981835e4b754aa
tihomir-petrovv <118721@students.ue-varna.bg>
2024-03-19 16:22:19 +0200

    Editing quizzes from group users

--- tppt-trivia/src/view/Groups/GroupParticipantsQuizzes/GroupParticipantsQuizzes.jsx
+++ tppt-trivia/src/view/Groups/GroupParticipantsQuizzes/GroupParticipantsQuizzes.jsx
@@ -8,33 +8,45 @@ export default function GroupParticipantsQuizzes() {
   const users = location.state.users;
   const [quizzes, setQuizzes] = useState([]);
   const navigate = useNavigate();
+  const [ filteredQuizzes, setFilteredQuizzes ] = useState([]);
 
   useEffect(() => {
-    Object.values(users).map((user) => {
-      takeAllQuizzesByGroupUsers(user.uid).then((quizzes) => {
-        setQuizzes(quizzes);
-      });
+    takeAllQuizzesByGroupUsers().then((quizzes) => {
+      setQuizzes(quizzes)
     });
   }, [users]);
 
+  useEffect(() => {
+    if (!users || !quizzes.length) return;
+  
+    const filteredQuizzes = Object.values(users).flatMap((user) =>
+      quizzes.filter((quiz) => quiz.creatorId === user.uid)
+    );
+  
+    setFilteredQuizzes(filteredQuizzes);
+  }, [quizzes, users]);
+  
+
   const editQuiz = (quiz) => {
-    navigate(`/edit-quiz/${quiz.id}`, {state: {quiz}});
+    navigate(`/edit-quiz/${quiz.id}`, { state: { quiz } });
   };
-console.log(quizzes)
+
+  //console.log(filteredQuizzes)
+
   return (
     <div>
       <h1>Quizzes created by group participants:</h1>
-      {Object.values(quizzes).map((quiz) => {
+      {Object.values(filteredQuizzes).map((quiz) => {
         return (
-        <div key={quiz.id}>
+          <div key={quiz.id}>
             <h3>Quiz title: {quiz.title}</h3>
             <p>Category: {quiz.category}</p>
             <p>Created on: {quiz.createdOn}</p>
             <p>Time limit: {quiz.timeLimit}</p>
             <p>Time limit: {quiz.username}</p>
             <button onClick={() => editQuiz(quiz)}>Edit quiz</button>
-        </div>
-        )
+          </div>
+        );
       })}
     </div>
   );
