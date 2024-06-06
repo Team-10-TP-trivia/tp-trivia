9b8f4924faf0fabc31e6b7ee6d7991f105d74960
tihomir-petrovv <118721@students.ue-varna.bg>
2024-06-06 17:31:50 +0300

    Changing home page, adding standing out words and making them clockwise. Making them clockwise also for screens with resolution less than 900px

--- tppt-trivia/src/view/Home/Home.jsx
+++ tppt-trivia/src/view/Home/Home.jsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from 'react';
-import DemoQuizzes from '../Demo/DemoQuiz';
-import './Home.css'; 
-import { getAllUsers } from '../../services/AdminServices/admin-services';
+import { useContext, useEffect, useState } from "react";
+import DemoQuizzes from "../Demo/DemoQuiz";
+import "./Home.css";
+import { getAllUsers } from "../../services/AdminServices/admin-services";
 import { FaCrown } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../context/appContext";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdGroup } from "react-icons/md";
+import { PiTimerFill } from "react-icons/pi";
+import { IoAccessibility } from "react-icons/io5";
+import { BiLayout } from "react-icons/bi";
+import { IoShareSocial } from "react-icons/io5";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 export default function Home() {
-  const [ users, setUsers ] = useState([]);
-  const [ userGlobalScores, setUserGlobalScores ] = useState([]);
+  const { user } = useContext(AppContext);
+  const [users, setUsers] = useState([]);
+  const [userGlobalScores, setUserGlobalScores] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,25 +28,36 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-  if(users) {
-    const scores = users
-      .filter(user => user.takenQuizzes) // Filter users who have taken quizzes
-      .map(user => {
-        // Use reduce to sum the scores for each quiz
-        const totalScore = Object.values(user.takenQuizzes).reduce((total, quiz) => {
-          const quizScore = Object.values(quiz).reduce((acc, score) =>acc + score, 0);
-          return total + quizScore;
-        }, 0);
-        return { username: user.username,firstName: user.firstName, lastName: user.lastName, totalScore }; // Return an object that contains the username and the total score
-      });
+    if (users) {
+      const scores = users
+        .filter((user) => user.takenQuizzes) // Filter users who have taken quizzes
+        .map((user) => {
+          // Use reduce to sum the scores for each quiz
+          const totalScore = Object.values(user.takenQuizzes).reduce(
+            (total, quiz) => {
+              const quizScore = Object.values(quiz).reduce(
+                (acc, score) => acc + score,
+                0
+              );
+              return total + quizScore;
+            },
+            0
+          );
+          return {
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            totalScore,
+          }; // Return an object that contains the username and the total score
+        });
       const sortedScores = scores.sort((a, b) => b.totalScore - a.totalScore);
 
       // Take only the top 3 scores
       const topThreeScores = sortedScores.slice(0, 3);
-  
+
       setUserGlobalScores(topThreeScores);
-  }
-}, [users]);
+    }
+  }, [users]);
 
   return (
     <div className="home-container">
@@ -45,11 +65,69 @@ export default function Home() {
         <div className="hero-text">
           <h1>Experience a new era of Quiz Play</h1>
           <p>
-            TPPT Trivia is more than flashcards: it’s the #1 global learning
-            platform. Join our community of hundreds of learners using
-            TPPT Trivia practice tests and Expert Solutions to improve their grades and reach their goals.
+            TP-Trivia is more than flashcards: it’s the #1 global learning
+            platform. Join our community of hundreds of learners using TP-Trivia
+            practice tests and Expert Solutions to improve their grades and
+            reach their goals.
           </p>
-          <button className="sign-up-btn" onClick={() => navigate('/register')}>Sign up now</button>
+          {!user && (
+            <button
+              className="sign-up-btn"
+              onClick={() => navigate("/register")}
+            >
+              Get started <MdOutlineKeyboardArrowRight />
+            </button>
+          )}
+          <div id="stand-out-words">
+            <div>
+              <BiLayout
+                style={{
+                  marginRight: "5px",
+                }}
+              />
+              Interactive
+            </div>
+            <div>
+              <IoShareSocial
+                style={{
+                  marginRight: "5px",
+                }}
+              />
+              Social
+            </div>
+            <div>
+              <FaRegQuestionCircle
+                style={{
+                  marginRight: "5px",
+                }}
+              />
+              Questionnaires
+            </div>
+            <div>
+              <MdGroup
+                style={{
+                  marginRight: "5px",
+                }}
+              ></MdGroup>
+              Collaborative
+            </div>
+            <div>
+              <PiTimerFill
+                style={{
+                  marginRight: "5px",
+                }}
+              />
+              Real-time
+            </div>
+            <div>
+              <IoAccessibility
+                style={{
+                  marginRight: "5px",
+                }}
+              />
+              Accessible
+            </div>
+          </div>
         </div>
       </section>
 
@@ -57,8 +135,9 @@ export default function Home() {
         <div className="global-ranking-text">
           <h1>Track Your Global Ranking</h1>
           <p>
-            See how you stack up against learners from around the world on our global leaderboard. 
-            Challenge yourself to climb the ranks and achieve trivia mastery!
+            See how you stack up against learners from around the world on our
+            global leaderboard. Challenge yourself to climb the ranks and
+            achieve trivia mastery!
           </p>
         </div>
         <div className="global-ranking-table">
@@ -68,16 +147,18 @@ export default function Home() {
               <table>
                 <thead>
                   <tr>
-                    <th>Place</th>
+                    <th id="place">Place</th>
                     <th>First Name</th>
                     <th>Last Name</th>
-                    <th>Score</th>
+                    <th id="score">Score</th>
                   </tr>
                 </thead>
                 {userGlobalScores.map((user, index) => (
                   <tbody key={index}>
-                    <tr >
-                      <td>{index + 1} <FaCrown id={`crown${index+1}`}/></td>
+                    <tr>
+                      <td>
+                        {index + 1} <FaCrown id={`crown${index + 1}`} />
+                      </td>
                       <td>{user.firstName}</td>
                       <td>{user.lastName}</td>
                       <td>{user.totalScore} pt.</td>
