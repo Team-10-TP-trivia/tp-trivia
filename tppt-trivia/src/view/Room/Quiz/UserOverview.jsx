43e67e831f7b2637a5d189e8a3cb67d95cf56820
tihomir-petrovv <118721@students.ue-varna.bg>
2024-07-16 18:03:22 +0300

    Blocking users to go back to the quiz once they went to overview(finished the quiz)

--- tppt-trivia/src/view/Room/Quiz/UserOverview.jsx
+++ tppt-trivia/src/view/Room/Quiz/UserOverview.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 import { AppContext } from "../../../context/appContext";
 import { getUserQuiz } from "../../../services/QuizService/Quizzes";
+
+const history = createBrowserHistory();
 
 export default function UserOverview() {
   const { userData } = useContext(AppContext);
@@ -14,7 +17,7 @@ export default function UserOverview() {
   const demoQuizPoints = location.state.userPoints;
   const demoQuizRightAnswers = location.state.rightAnswers;
   const [userQuizResults, setUserQuizResults] = useState([]);
-  const [ percentage , setPercentage ] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
   useEffect(() => {
     if (!quizId || !userData) return;
@@ -22,7 +25,9 @@ export default function UserOverview() {
       try {
         const snapshot = await getUserQuiz(userData.username, quizId);
         setUserQuizResults(snapshot);
-        setPercentage(((snapshot.receivedPoints / snapshot.quizPoints) * 100).toFixed(2));
+        setPercentage(
+          ((snapshot.receivedPoints / snapshot.quizPoints) * 100).toFixed(2)
+        );
       } catch (error) {
         console.error("Error fetching user results:", error);
       }
@@ -31,59 +36,77 @@ export default function UserOverview() {
     fetchUserResults();
   }, [userData, quizId]);
 
-  if(!userData) {
-    return <div style={{
-      justifyContent: "center",
-      alignItems: "center",
-      height: "fit-content",
-      minHeight: "60vh",
-      margin: "20px",
-    }}>
-    <h1>Your results for quiz {quiz.title}</h1>
-    {userQuizResults && (
-      <div>
-        <p>
-          Right answers: {demoQuizRightAnswers} out of{" "}
-          {quizQuestions.length}
-        </p>
-        <p>
-          Points: {demoQuizPoints}
-        </p>
+  useEffect(() => {
+    const unblock = history.block((tx) => {
+      if (tx.action === 'POP') {
+        const allowNavigation = window.confirm("Are you sure you want to leave this page?");
+        if (!allowNavigation) {
+          history.go(1);
+        }
+      }
+    });
+
+    return () => {
+      unblock();
+    };
+  }, []);
+
+  if (!userData) {
+    return (
+      <div
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "fit-content",
+          minHeight: "60vh",
+          margin: "20px",
+        }}
+      >
+        <h1>Your results for quiz {quiz.title}</h1>
+        {userQuizResults && (
+          <div>
+            <p>
+              Right answers: {demoQuizRightAnswers} out of{" "}
+              {quizQuestions.length}
+            </p>
+            <p>Points: {demoQuizPoints}</p>
+          </div>
+        )}
+        {answers && (
+          <div>
+            <h2>Your answers:</h2>
+            {selectedAnswers &&
+              answers.map((answer, index) => {
+                const question = quizQuestions[index];
+                const selectedAnswer = selectedAnswers[index];
+                if (!selectedAnswer) {
+                  return (
+                    <div key={index}>
+                      <p>Question: {question.question}</p>
+                      <p>No answer selected</p>
+                    </div>
+                  );
+                }
+                const isCorrect = selectedAnswer.split("-")[1] === "true";
+                return (
+                  <div key={index}>
+                    <p>Question: {question.question}</p>
+                    <p style={{ color: isCorrect ? "green" : "red" }}>
+                      Answer: {selectedAnswer.split("-")[0]}{" "}
+                      {isCorrect ? "✅" : "❌"}
+                    </p>
+                    {isCorrect ? null : (
+                      <p>
+                        Right answer is: {answer.find((a) => a.isCorrect).text}
+                      </p>
+                    )}
+                  </div>
+                );
+              })}
+          </div>
+        )}
       </div>
-    )}
-    {answers && (
-      <div>
-        <h2>Your answers:</h2>
-        {selectedAnswers &&
-          answers.map((answer, index) => {
-            const question = quizQuestions[index];
-            const selectedAnswer = selectedAnswers[index];
-            if (!selectedAnswer) {
-              return (
-                <div key={index}>
-                  <p>Question: {question.question}</p>
-                  <p>No answer selected</p>
-                </div>
-              );
-            }
-            const isCorrect = selectedAnswer.split("-")[1] === "true";
-            return (
-              <div key={index}>
-                <p>Question: {question.question}</p>
-                <p style={{ color: isCorrect ? "green" : "red" }}>
-                  Answer: {selectedAnswer.split("-")[0]} {isCorrect ? "✅" : "❌"}
-                </p>
-                {isCorrect ? null : (
-                  <p>
-                    Right answer is: {answer.find((a) => a.isCorrect).text}
-                  </p>
-                )}
-              </div>
-            );
-          })}
-      </div>
-    )}
-  </div>
+    );
   }
 
   return (
@@ -121,7 +144,8 @@ export default function UserOverview() {
                 <div key={index}>
                   <p>Question: {question.question}</p>
                   <p style={{ color: isCorrect ? "green" : "red" }}>
-                    Your answer: {selectedAnswer.split("-")[0]} {isCorrect ? "✅" : "❌"}
+                    Your answer: {selectedAnswer.split("-")[0]}{" "}
+                    {isCorrect ? "✅" : "❌"}
                   </p>
                   {isCorrect ? null : (
                     <p>
